72f1d7b849d3a1739e1d6d636829d02d0fa5152f
MuhNaeem <32309405+MuhNaeem@users.noreply.github.com>
2019-03-09 22:34:43 +0500

    again

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,8 +74,8 @@ router.post("/register", (req, res) => {
         var transporter = nodemailer.createTransport({
           service: "Sendgrid",
           auth: {
-            user: "naeemfaheem010@gmail.com",
-            pass: "meharoom393"
+            user: "process.env.SENDGRID_USERNAME",
+            pass: "process.env.SENDGRID_PASSWORD"
           }
         });
         var mailOptions = {
